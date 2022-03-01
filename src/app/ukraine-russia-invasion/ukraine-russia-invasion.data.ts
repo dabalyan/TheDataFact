bb9a502c1ebe66360e896e385275f57c9d64f388
dabalyan <dabalyan@hotmail.com>
2022-03-01 21:37:04 +0100

    ukraine data latest update

--- src/app/ukraine-russia-invasion/ukraine-russia-invasion.data.ts
+++ src/app/ukraine-russia-invasion/ukraine-russia-invasion.data.ts
@@ -1,5 +1,125 @@
 export const INVASION_IMAGES = () => [
   {
+    'date': '2022-03-03T12:46:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/4/4f/2022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Added bombing of Izium'
+  },
+  {
+    'date': '2022-03-03T12:31:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220303124632%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Add scale'
+  },
+  {
+    'date': '2022-03-03T11:53:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220303123100%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Balakliya has been captured by Russia https://kh-news.net/mestnoe/item/21834-vojska-rf-voshli-v-balakleyu-vlasti-otkazalis-ot-sotrudnichestva-s-okkupantami.html'
+  },
+  {
+    'date': '2022-03-03T11:45:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220303115338%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': '(Lots of changes, information from the Institute for the study of war.) Kyiv; Russian lines extend to Horenychi. Vyshhorod not contested, Russians are more north towards Dymer. Outside of Makariv, Havronshchyna is held by Russians. Bucha is reported to be in full Ukrainian control. East of the Dnieper: Lines have not been reported to extend that close to Kiev, only up to 50 km. Flank extends through Kozelets (added), Bobrovytsia, and Makiivka (added). Updated frontline around Chernihiv. Konot...'
+  },
+  {
+    'date': '2022-03-03T11:22:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220303114501%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Fix Ternopil bombing location'
+  },
+  {
+    'date': '2022-03-02T23:46:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220303112255%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Voznesensk; bridge has been blown up as a defensive measure. Russians sent a lone army group with tanks and are south of the settlement.'
+  },
+  {
+    'date': '2022-03-02T23:36:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302234603%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Hostomel Airport is still under Russian control.'
+  },
+  {
+    'date': '2022-03-02T21:51:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302233652%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Changed Kamianka-Dniprovska to Russian controlled due to "territories around Zaphorizhzhia nuclear facility" being captured (Source: IAEA). Moved Enerhodar and power plant a bit and updated frontlines around them.'
+  },
+  {
+    'date': '2022-03-02T20:33:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302215100%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Kherson has been fully captured by Russia, military administration is being set up. https://twitter.com/mschwirtz/status/1499114918661996550 (New York Times reporter)'
+  },
+  {
+    'date': '2022-03-02T19:10:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302203327%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Add the village of Kruty'
+  },
+  {
+    'date': '2022-03-02T17:11:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302172133%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'IAEA confirms the Zaporizhzhia Plant has been captured by the Russians'
+  },
+  {
+    'date': '2022-03-02T17:05:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302170540%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Okay apparently the status of Kherson is still in dispute, but the battle for Mykolaiv has still begun'
+  },
+  {
+    'date': '2022-03-02T16:59:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302170519%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Kherson captured, Russians advance to Mykolaiv'
+  },
+  {
+    'date': '2022-03-02T16:58:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302165927%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Small fixes of vectors (Mariupol - Donetsk border), small frontline updates, added Hostomel airport icon, fixed a few icons on the wrong layer.'
+  },
+  {
+    'date': '2022-03-02T15:31:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302153355%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Translation test'
+  },
+  {
+    'date': '2022-03-02T11:48:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302153148%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Changed Konotop and Kharkiv to contested (Considering the mayor rejected surrender this implies Russians do not fully control the settlement https://www.ukrinform.net/rubric-ato/3417910-as-russian-invaders-demand-that-konotop-surrender-mayor-explicitly-defies-threats.html and troops have landed in central Kharkiv, fighting has occured)'
+  },
+  {
+    'date': '2022-03-02T10:00:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302102535%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Konotob once again reported to be under Ukrainian control'
+  },
+  {
+    'date': '2022-03-02T07:05:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302100013%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Trostianets fallen, Artsyz and Kanatovo airbase bombed, 1)https://nv.ua/ukr/ukraine/events/trostyanec-okupanti-povnistyu-zaynyali-misto-v-sumskiy-oblasti-novini-ukrajini-50221459.html 2) https://odessamedia.net/news/artsiz-bili-raketoj-komendantskij-chas-v-otg-prodlili-na-sutki 3)https://www.ukrinform.net/rubric-ato/3416670-russian-forces-shelling-kanatove-airfield-near-kropyvnytskyi.html'
+  },
+  {
+    'date': '2022-03-01T19:42:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220302070555%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Add Izmail radar facility airstrike https://www.tulceanoastra.ro/national/atac-cu-rachete-rusesti-la-15-km-de-granita-romaniei/'
+  },
+  {
+    'date': '2022-03-01T19:09:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220301194230%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Set Kherson as contested as Ukrainian officials only say Russians have entered, and they still say they have control of the administrative building. Safe to assume Antonivka is in Russian control. Nova Zbur\'ivka added to map (contested, no recent source). Updated frontline in Kherson. Fixed frontline near Kiev (north).'
+  },
+  {
+    'date': '2022-03-01T18:56:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220301190943%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Kherson captured, Russians repelled from Makariv and Borodyanka'
+  },
+  {
+    'date': '2022-03-01T18:39:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220301185622%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Advance on Bobrovytsia through Slabyn and Pakul'
+  },
+  {
+    'date': '2022-03-01T17:19:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220301183921%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Enerhodar has been surrounded; not clear if Kamianka-Dniprovska has been taken. There have been talks of peaceful surrender of the town, but for now blockades extend around the settlement.'
+  },
+  {
+    'date': '2022-03-01T17:10:00.000Z',
+    'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220301171933%212022_Russian_invasion_of_Ukraine.svg',
+    'comment': 'Fixed settlements west of Kiev, added Makariv and Borodyanka. Added bobrovytsya and updated location. Fixed location of Severodonetsk and surroundings. Updated frontlines around Severodonetsk. Updated Stanytsia Lunhanska (Shyrokyi has been taken). Updated Mariupol, which has now been sorrounded.'
+  },
+  {
     'date': '2022-03-01T10:47:00.000Z',
     'url': 'https://upload.wikimedia.org/wikipedia/commons/archive/4/4f/20220301105023%212022_Russian_invasion_of_Ukraine.svg',
     'comment': 'Small fix around Nova Zburkiva & updated frontline around Kherson (Airport has been taken). Small fix to Makariv, Chernihiv frontlines. Russian troops in Bobrovytsya, not sure from where they came. Other small vector fixes.'
